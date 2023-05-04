0cf2f50cb68e03eb9033b6f67e85623459c5a4ce
msdhanga <mohammaddhanga2@gmail.com>
2023-05-04 09:42:36 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -41,13 +41,14 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final titleController = TextEditingController();
   final amountController = TextEditingController();
+  final descriptionController = TextEditingController();
   List<Transaction> transaction = [
-    Transaction(id: 't1', title: 'Mount Abu', amount: 20000, date: DateTime.now()),
-    Transaction(id: 't2', title: 'Jaipur', amount: 40000, date: DateTime.now()),
+    Transaction(id: 't1', title: 'Mount Abu', amount: 20000, date: DateTime.now(),description: 'nice'),
+    Transaction(id: 't2', title: 'Jaipur', amount: 40000, date: DateTime.now(),description: 'good'),
   ];
 
-  void _addTransaction(String title, double amount) {
-    if (title.isEmpty || amount <= 0) {
+  void _addTransaction(String title, double amount,String Desc) {
+    if (title.isEmpty || amount <= 0 ) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
           content: Text('Please insert value in a fields'),
@@ -55,7 +56,8 @@ class _MyHomePageState extends State<MyHomePage> {
       );
       return;
     }
-    var tre = Transaction(id: 't3', title: title, amount: amount, date: DateTime.now());
+
+    var tre = Transaction(id: 't3', title: title, amount: amount, date: DateTime.now(),description: Desc);
 
     setState(() {
       transaction.add(tre);
@@ -63,6 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
     titleController.clear();
     amountController.clear();
+    descriptionController.clear();
   }
 
   @override
@@ -73,23 +76,52 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Column(
         children: [
-          TextFormField(
-            keyboardType: TextInputType.text,
-            controller: titleController,
-            decoration: InputDecoration(
-              hintText: 'Title',
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 30,vertical: 10),
+            child: TextFormField(
+              keyboardType: TextInputType.text,
+              controller: titleController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.location_city),
+                prefixIconColor: Colors.blue,
+                labelText: 'Title',
+                hintText: 'Title',
+              ),
             ),
           ),
-          TextFormField(
-            keyboardType: TextInputType.number,
-            controller: amountController,
-            decoration: InputDecoration(
-              hintText: 'Amount',
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 30,vertical: 10),
+            child: TextFormField(
+              keyboardType: TextInputType.number,
+              controller: amountController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.currency_rupee),
+                prefixIconColor: Colors.blue,
+                labelText: 'Amount',
+                hintText: 'Amount',
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 30,vertical: 10),
+            child: TextFormField(
+              maxLines: 3,
+              keyboardType: TextInputType.text,
+              controller: descriptionController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.description),
+                prefixIconColor: Colors.blue,
+                labelText: 'Description',
+                hintText: 'Enter Description',
+              ),
             ),
           ),
           ElevatedButton(
             onPressed: () {
-              _addTransaction(titleController.text, double.tryParse(amountController.text) ?? 0);
+              _addTransaction(titleController.text,double.tryParse(amountController.text) ?? 0,descriptionController.text);
             },
             child: Text('Submit'),
           ),
@@ -97,11 +129,17 @@ class _MyHomePageState extends State<MyHomePage> {
             child: ListView.builder(
               itemCount: transaction.length,
               itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  title: Text('${transaction[index].title}'),
-                  subtitle: Text('${DateFormat.yMMMd().format(transaction[index].date)}'),
-                  leading: Text('${transaction[index].id}'),
-                  trailing: Text('${transaction[index].amount}'),
+                return Card(
+                  elevation: 5,
+                  child: Column(
+                    children: [
+                      Text('${transaction[index].title}'),
+                      Text('${DateFormat.yMMMd().format(transaction[index].date)}'),
+                      Text('${transaction[index].id}'),
+                      Text('${transaction[index].amount}'),
+                      Text('${transaction[index].description}'),
+                    ],
+                  ),
                 );
               },
             ),
--- lib/model/transaction.dart
+++ lib/model/transaction.dart
@@ -3,11 +3,13 @@ class Transaction{
   final String title;
   final double amount;
   final DateTime date;
+  final String? description;
 
   Transaction({
     required this.id,
     required this.title,
     required this.amount,
     required this.date,
+    this.description,
  });
 }
